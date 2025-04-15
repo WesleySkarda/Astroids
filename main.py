e66a257c2207ad4f76fce0a942fe0d5d0e79bea6
WesleySkarda <Wesleykimballskarda@gmail.com>
2025-04-15 16:56:00 -0600

    added player to groups updatable and drawable

--- main.py
+++ main.py
@@ -6,16 +6,25 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame_clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+    
+
     dt = 0
     game_loop_bool = True
+
     while game_loop_bool:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,"black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = pygame_clock.tick(60)/ 1000
     print("Starting Asteroids!")
