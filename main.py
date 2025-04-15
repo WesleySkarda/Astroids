57b60cc7cace7c7713de4d36439d1259f26eaf78
WesleySkarda <Wesleykimballskarda@gmail.com>
2025-04-15 15:47:01 -0600

    Started working on astroids

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_loop_bool = True
+    while game_loop_bool:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,"black")
+        pygame.display.flip()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
