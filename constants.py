71e25f801dac26b2a8ad7b1b854f8d793b6e27ee
WesleySkarda <Wesleykimballskarda@gmail.com>
2025-04-15 16:33:23 -0600

    Updated main, circleshape, constants, abd player

--- constants.py
+++ constants.py
@@ -1,7 +1,15 @@
+#screen width and hight variables
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
+# basic variables for the astroids
 ASTEROID_MIN_RADIUS = 20
 ASTEROID_KINDS = 3
 ASTEROID_SPAWN_RATE = 0.8  # seconds
 ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
+
+#player variables
+PLAYER_RADIUS = 20
+PLAYER_COLOR = "white"
+LINE_WIDTH = 2
+PLAYER_TURN_SPEED = 300
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame_clock = pygame.time.Clock()
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+    dt = 0
     game_loop_bool = True
     while game_loop_bool:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                break
+                return
         pygame.Surface.fill(screen,"black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = pygame_clock.tick(60)/ 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
